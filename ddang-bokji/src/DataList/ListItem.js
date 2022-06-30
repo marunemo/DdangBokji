a4a7ab99fa9e144aabbc9bcaad4923ac0cb8bd4c
marunemo <chys3697@gmail.com>
2022-06-30 14:06:49 +0000

    create: temporary list item template

---
+++ ddang-bokji/src/DataList/ListItem.js
@@ -0,0 +1,17 @@
+import { Card } from 'antd';
+
+function ItemCard(props) {
+	return (
+		<Card
+			title={props.title}
+			hoverable
+		>
+			<Card.Meta
+				title={props.title}
+				description={props.description}
+			/>
+		</Card>
+	);
+}
+
+export default ItemCard;

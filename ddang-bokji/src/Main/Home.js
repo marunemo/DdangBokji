a1b0e477a1c17929de68078375425ce1218c07c6
marunemo <chys3697@gmail.com>
2022-07-02 14:08:23 +0000

    create: home menu template

--- ddang-bokji/src/Main/Home.js
+++ ddang-bokji/src/Main/Home.js
@@ -1,7 +1,69 @@
-function Home() {
+import React from 'react';
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+function HomeContainer() {
+	const headerMenu = ["Home"].map((option, key) => {
+		return ({
+			key: String(key),
+			label: option
+		});
+	})
+	
+	const sideMenu = ["Test"].map((option, key) => {
+		return ({
+			key: String(key),
+			icon: null,
+			label: option,
+		});
+	})
+	
 	return (
-		<p>Hello World!</p>
+		<Layout>
+			<Layout.Header className="header">
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					items={headerMenu}
+				/>
+			</Layout.Header>
+			<Layout>
+				<Layout.Sider style={styles.sider}>
+					<Menu
+						mode="inline"
+						style={styles.sideMenu}
+						items={sideMenu}
+					/>
+				</Layout.Sider>
+				<Layout style={{ padding: '0 24px 24px' }}>
+					<Breadcrumb style={{ margin: '16px 0' }}>
+						<Breadcrumb.Item>1</Breadcrumb.Item>
+						<Breadcrumb.Item>2</Breadcrumb.Item>
+						<Breadcrumb.Item>3</Breadcrumb.Item>
+					</Breadcrumb>
+					<Layout.Content style={styles.content}>
+						Content
+					</Layout.Content>
+				</Layout>
+			</Layout>
+		</Layout>
 	);
 }
 
-export default Home;
+export default HomeContainer;
+
+const styles = {
+	sider: {
+		background: '#fff',
+		width: 200
+	},
+	sideMenu: {
+		height: '100%',
+		borderRight: 0
+	},
+	content: {
+		background: '#fff',
+		padding: 24,
+		margin: 0,
+		minHeight: 280,
+	}
+}
--- ddang-bokji/src/index.js
+++ ddang-bokji/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TouristSpotList from './DataList/TouristSpot';
+import HomeContainer from './Main/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TouristSpotList />
+    <HomeContainer />
   </React.StrictMode>
 );
 

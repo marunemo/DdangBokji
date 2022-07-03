c4ec2d100be160ab598211eb4abff545a6401bf2
marunemo <chys3697@gmail.com>
2022-07-03 07:22:00 +0000

    temp: routing target

--- ddang-bokji/src/Main/Router.js
+++ ddang-bokji/src/Main/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeContainer from './Home';
+import SpotInfo from '../Utility/SpotInfo';
 
 function MainRouter() {
 	return (
@@ -9,6 +10,10 @@ function MainRouter() {
 				<Route
 					path="/"
 					element={<HomeContainer />}
+				/>
+				<Route
+					path="//:id"
+					element={<SpotInfo />}
 				/>
 			</Routes>
 		</BrowserRouter>

9422d6f0cce5ed3f549b5283301461ca149010e4
marunemo <chys3697@gmail.com>
2022-07-03 10:56:49 +0000

    temp: result template

--- ddang-bokji/src/DataList/TouristSpotInfo.js
+++ ddang-bokji/src/DataList/TouristSpotInfo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Layout, Image, Typography } from 'antd';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import LoadingSpin from '../Utility/LoadingSpin';
@@ -24,7 +25,29 @@ function TouristSpotInfo({ match }) {
 	if(!isLoaded)
 		return <LoadingSpin />;
 	
-	return JSON.stringify(spotInfo);
+	return (
+		<Layout>
+			<Layout.Content>
+				<Image
+					width="100%"
+					height="100%"
+					alt={spotInfo.phototitle}
+					src={spotInfo.image_file}
+					// Empty icons created by LAFS - Flaticon
+					fallback="https://cdn-icons.flaticon.com/png/512/3586/premium/3586675.png?token=exp=1656836791~hmac=9ab5c8883e35964c0a42820f9065b528"
+				/>
+				<Typography>
+					<Typography.Text>
+						이름 : {spotInfo.rel_instltnnm}\
+						주소 : {spotInfo.instltnpstn}\
+						전화 : {spotInfo.cntadr}
+					</Typography.Text>
+				</Typography>
+			</Layout.Content>
+			<Layout.Sider>
+			</Layout.Sider>
+		</Layout>
+	);
 }
 
 export default TouristSpotInfo;

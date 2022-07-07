826ef2a80b660f8b34d1c7a8e34e1b53fd3d1fe5
marunemo <chys3697@gmail.com>
2022-07-07 14:17:21 +0000

    fix: set free from dependency

--- ddang-bokji/src/Utility/KakaoMap.js
+++ ddang-bokji/src/Utility/KakaoMap.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function KakaoMap(props) {
 	const { kakao } = window;
 	const { address } = props;
 	
-	useEffect(() => {
-		const container = document.getElementById('map');
-		
+	const container = document.getElementById('map');
+	if(container) {
 		const geocoder = new kakao.maps.services.Geocoder();
 		geocoder.addressSearch(address, function(result, status) {
-		 	if (status === kakao.maps.services.Status.OK) {
+			if (status === kakao.maps.services.Status.OK) {
 				const options = {
 					center: new kakao.maps.LatLng(result[0].y, result[0].x),
 					level: 3
 				};
 				return new kakao.maps.Map(container, options);
 			}
-			
+
 			const options = {
 				center: new kakao.maps.LatLng(33.450701, 126.570667),
 				level: 13
@@ -24,7 +23,7 @@ function KakaoMap(props) {
 			const map = new kakao.maps.Map(container, options);
 			return map;
 		});
-	}, []);
+	}
 	
 	return (
 		<div id="map" style={{ width: '500px', height: '400px' }}></div>
